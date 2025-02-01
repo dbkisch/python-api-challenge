4a8a7c15c22dba095af91ba8915e91a64cbe57a9
dbkisch <deborahkisch@Deborahs-MacBook-Air.local>
2025-02-01 11:14:17 -0600

    completed exercises

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "42738fce68e87a0021b2734c12e4cae1"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "5db8e90452cc4e1e98344aef0921281b"
